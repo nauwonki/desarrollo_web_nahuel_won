73772ac3aa37634f3114fb6018a88fc3bcde3dd7
nauwonki <131418747+nauwonki@users.noreply.github.com>
2025-08-21 21:28:42 -0400

    chore: minor changes

--- Tarea1/tarea1_portada_js.js
+++ Tarea1/tarea1_portada_js.js
@@ -2,3 +2,8 @@ let avisoBtn = document.getElementById("agregar-aviso-btn");
 avisoBtn.addEventListener("click", () => {
     window.location.href = "tarea1_agregar_html.html";
 });
+
+let listadoBtn = document.getElementById("ver-listado-btn");
+listadoBtn.addEventListener("click", () => {
+    window.location.href = "tarea1_listado.html";
+});
